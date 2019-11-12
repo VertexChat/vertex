2c4385209b7dfe32f6235fa1388a6525e3ce4615
Morgan Reilly <morgan.reilly27@gmail.com>
2019-11-12 13:47:19 +0000

    Documenting code
    Adding some info to the main dart class for Vertex

--- vertex_ui/lib/main.dart
+++ vertex_ui/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(Vertex_UI()); // Vertex_UI -> App root call to
 
 /// App Root
-class Vertex_UI extends StatelessWidget { // App root
+class Vertex_UI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +13,9 @@ class Vertex_UI extends StatelessWidget { // App root
         /// App theme-ing here
         primarySwatch: Colors.blue,
       ),
-      home: VertexLanding(title: 'Vertex Landing Page'), /// Change this to splashscreen
+      home: VertexLanding(landingTitle: 'Vertex Landing Page'),
+
+      /// Change this to splash-screen
       /// Landing screen should come later after Login
     );
   }
@@ -22,66 +24,49 @@ class Vertex_UI extends StatelessWidget { // App root
 /// Each class defined below here is now a part of the App Root node
 /// VertexLanding is currently main landing page, meaning the App will
 /// load to that page.
+/// Stateful class --> Stateful widget.
 class VertexLanding extends StatefulWidget {
-  VertexLanding({Key key, this.title}) : super(key: key);
+  /// Home page of application.
+  /// Fields in Widget subclass always marked final
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  VertexLanding({Key key, this.landingTitle}) : super(key: key);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final String landingTitle;
 
   @override
   _VertexLandingState createState() => _VertexLandingState();
 }
 
+/// Stateless class
 class _VertexLandingState extends State<VertexLanding> {
   int _counter = 0;
 
+  /// Debugging variable used for counter
+
+  /// Debugging method used for initial setup of Flutter
   void _incrementCounter() {
+    /// setState calls build method below
     setState(() {
-      /// Debugging method used for initial setup of Flutter
       _counter++;
     });
   }
 
+  /// Build is run and rerun every time above method, setState, is called
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    /// Scaffold: framework which implements the basic material
+    /// design visual layout structure of the flutter app.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        /// Setting AppBar title here
+        title: Text(widget.landingTitle),
       ),
+
+      /// Center: A widget that centers its child within itself
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        /// Column: A Column is a widget used to display child widgets in a vertical manner.
+        /// We can update this to be whatever we like.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -98,7 +83,7 @@ class _VertexLandingState extends State<VertexLanding> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
