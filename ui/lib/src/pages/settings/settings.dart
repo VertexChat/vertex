59a5096c2cc2324db5737bdd6ccf9c3ff7a74805
Morgan Reilly <morgan.reilly27@gmail.com>
2020-01-11 03:06:25 +0000

    Settings Page Template
    Want to begin working on functionality, more design to be done but current barebones could be sufficiant

--- ui/lib/src/pages/settings/settings.dart
+++ ui/lib/src/pages/settings/settings.dart
@@ -4,11 +4,44 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 
 class AudioManager {
-  var inputDevice = ["USB Headset", "3.5mm Headset"];
-  var outputDevice = ["USB Speaker", "USB Headset"];
-  var minSensitivity = 0;
-  var maxSensitivity = 100;
+  String inputDevice;
+  String outputDevice;
+  final minSensitivity = 0;
+  final maxSensitivity = 100;
   var inputSensitivity;
+
+  // ignore: non_constant_identifier_names
+  get input_device {
+    return inputDevice;
+  }
+
+  // ignore: non_constant_identifier_names
+  set input_device(String inputDevice) {
+    this.inputDevice = inputDevice;
+  }
+
+  // ignore: non_constant_identifier_names
+  get output_device {
+    return outputDevice;
+  }
+
+  // ignore: non_constant_identifier_names
+  set output_device(String outputDevice) {
+    this.outputDevice = outputDevice;
+  }
+
+  // ignore: non_constant_identifier_names
+  get input_sensitivity {
+    return inputSensitivity;
+  }
+
+  // ignore: non_constant_identifier_names
+  set input_sensitivity(var inputSensitivity) {
+    if (inputSensitivity <= maxSensitivity &&
+        inputSensitivity >= minSensitivity) {
+      this.inputSensitivity = inputSensitivity;
+    }
+  }
 }
 
 class InterfaceManager {
@@ -18,7 +51,6 @@ class InterfaceManager {
 
 /// Settings page - Probably need to add a stateless widget ?
 class SettingsRoute extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     AudioManager audioManager = new AudioManager();
@@ -28,10 +60,8 @@ class SettingsRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Settings"),
       ),
-
       body: ListView.builder(
-        itemBuilder: (context, index){
-
+        itemBuilder: (context, index) {
           return Container(
             decoration: new BoxDecoration(
               border: new Border.all(color: Colors.grey[500]),
@@ -39,6 +69,7 @@ class SettingsRoute extends StatelessWidget {
             ),
             child: new Column(
               children: <Widget>[
+                getAudioTitle(context),
                 getAudioWidget(context),
               ],
             ),
@@ -47,31 +78,52 @@ class SettingsRoute extends StatelessWidget {
       ),
     );
   }
+
   /// Widget for handling the audio tab
   Widget getAudioWidget(BuildContext context) {
     return new Row(
       children: <Widget>[
-        new Container(height: 100.0, width: 10.0, color: Colors.blue),
-        new Container(height: 100.0, width: 10.0, color: Colors.red),
-        new Container(
-          child: new Text("Audio", textAlign: TextAlign.start,
-            style: TextStyle(
-              fontFamily: 'Bold',
-              fontSize: 18.0,
-              color: Colors.black),
-          ),
-          decoration: new BoxDecoration(
-            color: Colors.amber,
-          ),
-          height: 100.0,
+        new Row(
+          children: <Widget>[
+            new Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: Text("Row 0 Container 0"),
+            ),
+            new Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: Text("Row 0 Container 1"),
+            ),
+          ],
         ),
         Spacer(),
       ],
     );
   }
+
+  /// @param BuildContext -- reference to location of Widget within tree of all Widgets
+  Widget getAudioTitle(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Container(
+          child: new Text(
+            "Audio Settings",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+                fontFamily: 'Bold', fontSize: 18.0, color: Colors.black),
+          ),
+          decoration: new BoxDecoration(
+            color: Colors.greenAccent,
+          ),
+          height: 100.0,
+        ),
+      ],
+    );
+  }
 }
-
-
 
 /// Method for settings choices
 void choiceAction(String choice) {
