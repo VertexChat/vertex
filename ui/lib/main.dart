35095d3f2eeeeaee97b66da50a1294684719388c
butlawr <cathalbutler@outlook.com>
2020-01-27 21:43:28 +0000

    Updated .gitignore and updated user model
    Added two new ignore to prevent unneeded files being added to the repo as they are auto generated when the application is ran.
    + .flutter-plugins
    + .flutter-plugins-dependencies
    * Updated user model class

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:vertex_ui/src/pages/settings_page.dart';
 import 'package:vertex_ui/src/pages/video_call/connect_call_page.dart';
 import 'package:vertex_ui/src/pages/login_page.dart';
 import 'package:vertex_ui/src/pages/register_page.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 /// Call to run App Root (App starts here)
 void main() => runApp(UI()); // Vertex_UI -> App root call to
@@ -15,26 +16,37 @@ class UI extends StatefulWidget {
 
 /// App Root
 class _UIState extends State<UI> {
+
   @override
-  void initState() {
+  void initState(){
     super.initState();
   }
+
+  Brightness brightness;
 
   @override
   Widget build(BuildContext context) {
     /// MaterialApp is the base Widget for your Flutter Application
     /// Gives us access to routing, context, and meta info functionality.
-    return MaterialApp(
-      title: 'Vertex',
-      theme: ThemeData(brightness: Brightness.dark),
-      home: VertexHomePage(title: 'Welcome Home'),
-      // TODO: ${username}
-      debugShowCheckedModeBanner: false,
-      // Remove debug banner
-      //Login route
-      routes: <String, WidgetBuilder>{
-        '/login': (BuildContext context) => new LoginPage(),
-        '/register': (BuildContext context) => new RegisterPage()
+    return new DynamicTheme(
+      defaultBrightness: Brightness.dark,
+      data: (brightness) => new ThemeData(
+        brightness: brightness,
+      ),
+      themedWidgetBuilder: (context, theme) {
+        return new MaterialApp(
+          title: 'Vertex',
+          theme: ThemeData(
+              brightness: Brightness.dark
+          ),
+          home: VertexHomePage(title: 'Welcome Home'), // TODO: ${username}
+          debugShowCheckedModeBanner: false, // Remove debug banner
+          //Login route
+          routes: <String, WidgetBuilder>{
+            '/login': (BuildContext context) => new LoginPage(),
+            '/register': (BuildContext context) => new RegisterPage()
+          },
+        );
       },
     );
   }
--- ui/lib/src/models/user.dart
+++ ui/lib/src/models/user.dart
@@ -1,21 +1,33 @@
+// https://dart.dev/tutorials/web/fetch-data
+
 class User {
+  //Variables
   String _email;
   String _password;
+  String _displayName;
+  String _groups;
 
   User.map(dynamic obj) {
     this._email = obj['email'];
     this._password = obj['password'];
+    this._displayName = obj['displayName'];
+    this._groups = obj['groups'];
   }
 
   String get email => _email;
 
   String get password => _password;
 
+  String get displayName => _displayName;
+
+  String get groups => _groups;
+
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map["email"] = _email;
     map["password"] = _password;
-
+    map['displayName'] = _displayName;
+    map['groups'] = _groups;
     return map;
-  }
+  } //End
 } //End class
