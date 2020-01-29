7e5e9fe8843b5189d772c146bd30074786c8cd13
Morgan Reilly <morgan.reilly27@gmail.com>
2020-01-29 23:08:10 +0000

    Update Database backup
    + Use cases added

--- vertexdb.sql
+++ vertexdb.sql
@@ -49,11 +49,11 @@ CREATE TABLE attatchment
     file_size INTEGER(64) NOT NULL,
     file_url VARCHAR(255) NOT NULL,
 
-    PRIMARY KEY (attatchment_0id  
-)
+    PRIMARY KEY (attatchment_id),
+    FOREIGN KEY(message_id) REFERENCES message(message_id)  
+) ENGINE = INNODB;
 
-
-/* Testing user input */
+/* Create user */
 select * from user;
 insert into user(user_name, password_hash, password_salt, display_name)
     values ('mreilly', 'foo1', 'bar2', 'mreilly');
@@ -61,9 +61,26 @@ insert into user(user_name, password_hash, password_salt, display_name)
     values ('cbutler', 'foo1', 'bar2', 'cbutler');
 insert into user(user_name, password_hash, password_salt, display_name)
     values ('dneilan', 'foo1', 'bar2', 'dneilan');
+insert into user(user_name, password_hash, password_salt, display_name)
+    values ('user1', 'foo1', 'bar2', 'exampleUser');
 select * from user;
 
-/* Testing channel input */
+/* Update user */
+select * from user;
+update user set user_name = 'morganreilly' where user_id = 1;
+update user set password_hash = 'aFc334', password_salt = 'bb4hsg3' where user_id = 1;
+update user set display_name = 'annie-hash' where user_id = 1;
+select * from user;
+
+/* Delete user */
+select * from user;
+delete from user where user_id = 4;
+select * from user;
+
+/* Read user by id */
+select * from user where user_id = 2;
+
+/* Create channel input */
 select * from channel;
 insert into channel(channel_name, user_id, channel_capacity, channel_type, channel_position)
     values ('Development', 0001, 20, 'TEXT', 1);
@@ -80,12 +97,60 @@ insert into channel(channel_name, user_id, channel_capacity, channel_type, chann
     values ('Gaming', 3, 20, 'VOICE', 1);
 select * from channel;
 
-/* Testing message input */
+/* Create message input */
 select * from message;
 insert into message(channel_id, user_id, message_content) 
-    values (3, 1, 'Hello, word');
+    values (3, 1, 'Hello, world');
 insert into message(channel_id, user_id, message_content) 
-    values (3, 2, 'Hello, word');
+    values (3, 2, 'Hello, world');
 insert into message(channel_id, user_id, message_content) 
-    values (1, 3, 'Hello, word');
+    values (1, 3, 'Hello, world');
 select * from message;
+
+
+/* Create attatchment input 
+Note -- This probably may change to allow for file inputs
+*/
+select * from attatchment;
+insert into attatchment(file_name, message_id, file_size, file_url)
+    values ('Example_file.jpg', 3, 1064, './home/pictures');
+insert into attatchment(file_name, message_id, file_size, file_url)
+    values ('Example_file2.jpg', 6, 10641064, './home/pictures');
+insert into attatchment(file_name, message_id, file_size, file_url)
+    values ('Example_file2.jpg', 1, 10641064, './home/pictures');
+select * from attatchment;
+
+/* Case 1:
+Display user name, display name, associated channels for all users
+*/
+select usr.user_name, usr.display_name, chnl.channel_name from user usr inner join channel chnl on usr.user_id = chnl.user_id;  
+
+/* Case 1.1:
+Display user name, display name, associated channels for specific user
+*/
+select usr.user_name, usr.display_name, chnl.channel_name from user usr inner join channel chnl on usr.user_id = chnl.user_id where usr.user_id = 1;  
+
+/* Case 2.1:
+Display all messages by all users by display name along with time sent for a all channels
+*/
+select usr.display_name, chnl.channel_name, msg.message_content, msg.message_timestamp from user usr inner join channel chnl on usr.user_id = chnl.user_id inner join message msg on chnl.user_id = msg.user_id;
+
+/* Case 2.2:
+Display all messages for specific user by display name along with time sent for a all channels
+*/
+select usr.display_name, chnl.channel_name, msg.message_content, msg.message_timestamp from user usr inner join channel chnl on usr.user_id = chnl.user_id inner join message msg on chnl.user_id = msg.user_id where usr.user_id = 1;
+
+/* Case 2.3:
+Display all messages for specific user by display name along with time sent for a specific channel
+*/
+select usr.display_name, chnl.channel_name, msg.message_content, msg.message_timestamp from user usr inner join channel chnl on usr.user_id = chnl.user_id inner join message msg on chnl.user_id = msg.user_id where usr.user_id = 1 and chnl.channel_name = 'Development';
+
+/* Case 3:
+Display all files sent by all users in all channels
+*/
+select usr.user_name, chnl.channel_name, att.file_name from user usr inner join channel chnl on usr.user_id = chnl.user_id inner join message msg on chnl.user_id = msg.user_id inner join attatchment att on msg.message_id = att.message_id;
+
+/* Case 3.1:
+Display all files sent by specific user in all channels
+*/
+select usr.user_name, chnl.channel_name, att.file_name from user usr inner join channel chnl on usr.user_id = chnl.user_id inner join message msg on chnl.user_id = msg.user_id inner join attatchment att on msg.message_id = att.message_id where usr.user_id = 1;
