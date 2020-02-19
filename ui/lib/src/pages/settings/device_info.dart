f0e0505bd8937bf64fa76e503c870c079578c67c
Cathal <cathalbutler@outlook.com>
2020-02-19 17:54:44 +0000

    Fixed a bug with async call
    * Updated _fetchPackageInfo method to now update state when information about the device is returned. This fixed a bug with the widget builder reporting a null values on strings

--- ui/lib/src/pages/settings/device_info.dart
+++ ui/lib/src/pages/settings/device_info.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:device_info/device_info.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 /// Class which gets information about the device it is currently on about and builds
@@ -21,19 +22,27 @@ class _DeviceInfoState extends State<DeviceInfo> {
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   AndroidDeviceInfo _androidInfo;
   IosDeviceInfo _iosDeviceInfo;
-  bool isWeb = false;
+  bool _isWeb = false;
+  String _name, _device, _version;
 
-  Future fetchDeviceInfo() async {
+  Future _fetchDeviceInfo() async {
     if (kIsWeb) {
       // App is running on the web
       // This is needed as a user can resize the webpage and the UI will scale for mobile
       // causing this class to look for mobile platform information
-      isWeb = true;
+      _isWeb = true;
       return;
     } else if (Platform.isAndroid) {
+      // Get info about device
       _androidInfo = await deviceInfo.androidInfo;
+      setState(() {
+        _device = _androidInfo.device;
+        _version = _androidInfo.version.toString();
+      });
     } else if (Platform.isIOS) {
+      // Get info about device
       _iosDeviceInfo = await deviceInfo.iosInfo;
+      setState(() => _name = _iosDeviceInfo.name);
     } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
       //TODO:
     } else {
@@ -43,37 +52,33 @@ class _DeviceInfoState extends State<DeviceInfo> {
 
   @override
   void initState() {
-    fetchDeviceInfo().whenComplete(() => print("Complete"));
     super.initState();
+    _fetchDeviceInfo();
   } //End function
 
   Widget _getDeviceLayout() {
-    if (isWeb) {
+    if (_isWeb) {
       return ListTile(
         leading: Icon(Icons.web),
         title: Text("Applcation is running on a webrower"),
       );
     } else {
-        return Column(
-          children: <Widget>[
-            ListTile(
-              leading: Platform.isAndroid
-                  ? Icon(Icons.android)
-                  : Icon(FontAwesomeIcons.apple),
-              title: Text("Platform",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              trailing: Platform.isAndroid
-                  ? Text(_androidInfo.device)
-                  : Text(_iosDeviceInfo.name),
-            ),
-            ListTile(
-              title: Text("OS", style: TextStyle(fontWeight: FontWeight.bold)),
-              trailing: Platform.isAndroid
-                  ? Text(_androidInfo.version.toString())
-                  : Text(_iosDeviceInfo.name),
-            ),
-          ],
-        );
+      return Column(
+        children: <Widget>[
+          ListTile(
+            leading: Platform.isAndroid
+                ? Icon(Icons.android)
+                : Icon(FontAwesomeIcons.apple),
+            title:
+                Text("Platform", style: TextStyle(fontWeight: FontWeight.bold)),
+            trailing: Platform.isAndroid ? Text(_device) : Text(_name),
+          ),
+          ListTile(
+            title: Text("OS", style: TextStyle(fontWeight: FontWeight.bold)),
+            trailing: Platform.isAndroid ? Text(_version) : Text(_name),
+          ),
+        ],
+      );
     } //end if else
   } //End get widget function
 
